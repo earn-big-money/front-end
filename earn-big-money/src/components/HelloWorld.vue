04095f8a1c338eb031b3ccd6dd1aad03f95ec944
ddghost <544011312@qq.com>
2019-03-17 23:55:41 +0800

    init project

---
+++ earn-big-money/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="example-1">
+    <div id="title">
+    </div>
+    <div>
+      <input id="search" placeholder="搜索" v-model="content"></input>
+      <button v-on:click="testApi()">Search</button>
+    </div>
+    <div id="con">
+      <textarea v-model="msg" rows="30" cols="100"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data: function() {
+    return {
+      msg: "",
+      content: "",
+      pages: 1,
+      contentTag: "",
+      currentPage: 1
+    };
+  },
+  methods: {
+    testApi: function() {
+      console.log("http://localhost:3000/" + this.content );
+      this.$http.get("http://localhost:3000/" + this.content ).then(
+        function(res) {
+          if(res.ok) {
+            this.msg = JSON.stringify(res.data, null, 4);
+          }
+        }, function() {
+          alert("error");
+        });
+    },
+  }
+}
+</script>
+
+<style>
+#con {
+  margin-top: 10px;
+}
+</style>
