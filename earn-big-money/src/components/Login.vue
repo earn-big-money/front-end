aca4505bc6f8a28c3e9debb3363e6ecdc10dd2ef
wei49 <448644689@qq.com>
2019-04-08 15:45:34 +0800

    login and register

---
+++ earn-big-money/src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div id='header'>
+            <div id="title">
+                EBM-赚闲钱
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="input-container login-container">
+                <div class="subtitle">Login</div>
+                <form class="form-container">
+                    <div class="tips">提示</div>
+                    <div class="form-group">
+                        <!-- <label for="username_input">用户名</label> -->
+                        <input id="username-input" type="text" v-model="username" class="form-input" placeholder="用户名"/>
+                    </div>
+                    <div class="form-group">
+                        <!-- <label for="password">Password</label> -->
+                        <input id="password-input" type="password" v-model="password"  class="form-input" placeholder="密码"/>
+
+                    </div>
+                    <!-- <div class="form-group">
+                        <label>下次自动登录</label>
+                    </div> -->
+                    <div class="form-group">
+                        <button class="btn login-btn">登录</button>
+                        
+                    </div>
+                    <div class="form-group">
+                        <router-link to="Register" class="btn link-btn">注册</router-link>
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+ 
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            username: '',
+            password: '',
+        }
+    },
+};
+</script>
+
+<style>
+@import "../assets/css/LoginAndRegister.css" 
+</style>
--- earn-big-money/src/router/index.js
+++ earn-big-money/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+
+    {
+      path: '/Register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
