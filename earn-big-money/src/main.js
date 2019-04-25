7a462814794b03ef9eb82aebafcad4807e7fadc9
emilyblues <liaolei3@mail2.sysu.edu.cn>
2019-04-25 12:37:38 +0800

    add some modules

--- earn-big-money/src/main.js
+++ earn-big-money/src/main.js
@@ -1,16 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
-Vue.use(VueResource)
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+Vue.use(VueResource)
+Vue.use(ElementUI)
+library.add(faCoffee)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
