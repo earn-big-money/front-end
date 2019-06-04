a0f5d4329a821fac74ec97b7503e085699c0ff94
ddghost <544011312@qq.com>
2019-06-04 11:14:31 +0800

    update taskDetail and CreateTask

--- earn-big-money/src/components/TaskDetail/TaskDetail.vue
+++ earn-big-money/src/components/TaskDetail/TaskDetail.vue
@@ -1,100 +1,53 @@
 <template>
-	<div style="width: 600px; margin: 0 auto;" >
-		<el-container>
-		  <el-header>{{task.taskName}}</el-header>
-		  <el-container style="height:50px">
-		    <el-aside width="200px">任务类型:</el-aside>
-		    <el-main>{{task.taskType}}</el-main>
-		  </el-container>
-
-			<el-container style="height:50px">
-		    <el-aside width="200px">任务发布者:</el-aside>
-		    <el-main>{{task.creater}}</el-main>
-		  </el-container>
-
-			<el-container style="height:50px">
-		    <el-aside width="200px">任务时间:</el-aside>
-		    <el-main>{{task.startTime}} ~ {{task.endTime}}</el-main>
-		  </el-container>
-
-			<el-container style="height:50px">
-		    <el-aside width="200px">剩余参加人数:</el-aside>
-		    <el-main>{{task.paticipantNum}}</el-main>
-		  </el-container>
-			<el-container style="height:50px">
-		    <el-aside width="200px">参与薪酬:</el-aside>
-		    <el-main>{{task.taskWage}}元</el-main>
-		  </el-container>
-
-			<el-container>
-		    <el-header style="text-align:left">任务内容:</el-header>
-		    <el-main style="text-align:left">
-					{{task.taskContent}}
-				</el-main>
-		  </el-container>
-		</el-container>
-		<el-button type="primary">我要报名</el-button>
-		<router-link :to="{path:'/'}">
-			<el-button type="primary">返回</el-button>
-		</router-link>
+	<div>
+		<el-menu :default-active="'checkInfo'"
+		  class="el-menu-demo"
+		  mode="horizontal"
+		  background-color="#545c64"
+		  text-color="#fff"
+		  active-text-color="#ffd04b"
+			@select="handleSelect">
+		  <el-menu-item index="checkInfo">任务信息</el-menu-item>
+		  <el-submenu index="checkPaticipant" v-if="isCreater">
+		    <template slot="title" >查看参加者信息</template>
+		    <el-menu-item index="checkDone">已完成</el-menu-item>
+		    <el-menu-item index="checkUnDone">未完成</el-menu-item>
+		  </el-submenu>
+		</el-menu>
+		<v-taskInfo v-if='isShowInfo' ref='taskInfo'></v-taskInfo>
 	</div>
 </template>
 
-
-<style>
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-		width: 200px;
-    text-align: center;
-		line-height: 50px
-  }
-
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-		padding: 0 40px;
-    text-align: center;
-		line-height: 50px
-  }
-
-</style>
-
 <script>
+	import taskInfo from './taskInfo.vue'
 	export default {
-	data() {
-	  return {
-			task: {
-				taskName:'hello world',
-				taskType:'问卷',
-				creater:'ddghost',
-				startTime:'2019-07-13',
-				endTime:'2019-08-12',
-				taskContent:'tmp',
-				paticipantNum:10,
-				taskWage:1
-			},
-		}
+	components: {
+		"v-taskInfo":taskInfo,
+
 	},
+  data() {
+    return {
+			isShowInfo:true,
+			userId:'',
+			task:{},
+			isCreater:false
+    };
+  },
 	mounted: function () {
-		console.log(this.$route.params.duty)
-		console.log(this.$route.params.uid)
-		//console.log(this.$route.params)
+		this.userId = this.$refs.taskInfo.userId
+		this.task = this.$refs.taskInfo.task
+		this.isCreater = (this.userId == this.task.creater)
+		//console.log()
 	},
-
-	methods: {
-
-
-	}
-
-
-
+  methods: {
+		handleSelect(key, keyPath) {
+			if(key == 'checkInfo'){
+				this.isShowInfo = true
+			}
+			else{
+				this.isShowInfo = false
+			}
+    }
+  }
 }
 </script>
